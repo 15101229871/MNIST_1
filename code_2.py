6d13963871de93907b6679eb56f51115f7100246
Zhongjie Ma <66350599+15101229871@users.noreply.github.com>
2020-07-24 11:15:30 +0800

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 import input_data
 import matplotlib.pyplot as plt
+# 启用动态图机制，不可删除
+tf.enable_eager_execution()
 
 mnist_data = input_data.read_data_sets('mnist_data', one_hot=False)
 
@@ -17,7 +19,7 @@ out = tf.keras.layers.Dense(10, activation='softmax')(dense)
 
 model = tf.keras.Model(inputs=input_, outputs=out)
 model.summary()
-model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy,optimizer='adam', metrics=['accuracy'])
 
 model.fit(x=train_images, y=train_labels, epochs=5)
 
@@ -26,5 +28,11 @@ for i in range(10):
     img = np.reshape(test_images[i], (28, 28))
     lab = test_labels[i]
     print('真实标签: ', lab, '， 网络预测: ', np.argmax(pred.numpy()))
+    '''
+    import numpy as np
+    a = np.array([3, 1, 2, 4, 6, 1])
+    b=np.argmax(a)#取出a中元素最大值所对应的索引，此时最大值位6，其对应的位置索引值为4，（索引值默认从0开始）
+    print(b)#4
+    '''
     plt.imshow(img)
     plt.show()
